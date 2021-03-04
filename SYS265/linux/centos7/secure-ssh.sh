af4c81d86030cadcb497fedd72cb6481c8158e68
CyPH3RSkULL5 <noah.beckman@comcast.net>
2021-03-04 12:02:08 -0500

    added beginning of ssh script

---
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -0,0 +1,6 @@
+#secure-ssh
+#author noah
+#Create a new ssh user using $1 parameter
+#adds a public key from the local reop or curled form the remote repo
+# removes root ability to ssh in
+# code goes here
